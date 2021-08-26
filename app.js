cd55ed0b89c41df533e28b22bcdc76209e69e327
HungriestBen <benjamin.ton@gmail.com>
2021-08-26 13:44:18 +1000

    Functional TTT game

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var allButtons = document.querySelectorAll(".button");
-var button1 = document.querySelector(".input")
 var turnBanner = document.querySelector(".turnbanner")
+var resetButton = document.querySelector(".resetButton")
 //How to determine player turn
 //Odd and even numbers?
 
@@ -22,8 +22,26 @@ var turnCounter = 0
 for (i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener("click", handleClick);
 }
+//Create add listener for reset button
+resetButton.addEventListener("click", clickReset);
+//Function for reset
+
+function clickReset () {
+    for (i = 0; i < allButtons.length; i++) {
+        allButtons[i].className = "button"
+    }
+    for (i = 0; i < allButtons.length; i++) {
+        allButtons[i].textContent = ""
+    }
+    turnCounter = 0
+    console.log("Click reset clicked")
+    for (i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener("click", handleClick);
+    }
+}
 
 
+//Function for all buttons
 function handleClick (event) {
     if (event.target.textContent !== "") {
         return;
@@ -51,16 +69,21 @@ function handleClick (event) {
 /////////
 // winCondition1 = 
 
-
+//Win condition function....
 function checkWinConditions(player) {
     var playerButton = player + "turn button";
     console.log(playerButton);
     var winText = player.toUpperCase() + " WINS!";
-//1
-    if (allButtons[0].className === playerButton && 
+
+//1 // If buttons 1,2,3 = function(x || o) win display appears at the bottom
+    // Now figure out how to end the game
+    if (allButtons[0].className === playerButton &&  
         allButtons[1].className === playerButton && 
         allButtons[2].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
     } 
 //2
@@ -68,6 +91,9 @@ function checkWinConditions(player) {
         allButtons[4].className === playerButton && 
         allButtons[5].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
     }
 //3
@@ -75,6 +101,9 @@ function checkWinConditions(player) {
         allButtons[7].className === playerButton && 
         allButtons[8].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
     }
 //4 
@@ -82,6 +111,9 @@ function checkWinConditions(player) {
         allButtons[3].className === playerButton && 
         allButtons[6].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
     }
 //5
@@ -89,6 +121,9 @@ function checkWinConditions(player) {
         allButtons[4].className === playerButton && 
         allButtons[7].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
         }
 //6    
@@ -96,6 +131,9 @@ function checkWinConditions(player) {
         allButtons[5].className === playerButton && 
         allButtons[8].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
         }
 //7
@@ -103,6 +141,9 @@ function checkWinConditions(player) {
         allButtons[4].className === playerButton && 
         allButtons[8].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
         }
 //8
@@ -110,31 +151,44 @@ function checkWinConditions(player) {
         allButtons[4].className === playerButton && 
         allButtons[6].className === playerButton) {
             turnBanner.textContent = winText;
+            for (i = 0; i < allButtons.length; i++) {
+                allButtons[i].removeEventListener("click", handleClick);
+            }
             console.log("POTATO");
         }
 
-        // allButtons.forEach(function(whatever) {
-        //     (whatever).className !== "button"
+        // allButtons.forEach(function(element) {
+        //     (element).className !== "button"
         //   });
      
-        if (allButtons[0].className !== "button" &&
-            allButtons[1].className !== "button" &&
-            allButtons[2].className !== "button" &&
-            allButtons[3].className !== "button" &&
-            allButtons[4].className !== "button" &&
-            allButtons[5].className !== "button" &&
-            allButtons[6].className !== "button" &&
-            allButtons[7].className !== "button" &&
-            allButtons[8].className !== "button") {
-                turnBanner.textContent = "DRAW"
-            }
-            
-        
-        
+    else if (allButtons[0].className !== "button" &&
+        allButtons[1].className !== "button" &&
+        allButtons[2].className !== "button" &&
+        allButtons[3].className !== "button" &&
+        allButtons[4].className !== "button" &&
+        allButtons[5].className !== "button" &&
+        allButtons[6].className !== "button" &&
+        allButtons[7].className !== "button" &&
+        allButtons[8].className !== "button") {
+            // CREATE AN IF STATEMENT FOR EACH WIN CONDITION AND RETURN.
+            // COULD SEPERATE DRAW FUNCTION TO RETURN.
+            // removeEventListeners on win
+            turnBanner.textContent = "DRAW"
+        }   //If win condition is not CheckWinOrDraw then it's a draw
 }
 
+
+    function checkWinOrDraw () {
+        checkWinConditions("x");
+        checkWinConditions("o");
+    }
         // Then X wins
 
         // document.querySelector(".turnbanner").textContent = "X WINS"
 
 // are all id #xturn or #oturn
+
+//BOOLEAN IF THE GAME IS STILL RUNNING
+//EG. IF WIN CONDITION MET === TRUE - PAUSE ALL DIV EVENT LISTENERS?
+//
+//I HAVE A FUNCTION LOOP TO CHECK WIN CONDITION
